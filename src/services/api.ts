abe7ce9db02f4c070a115952cc16e3a894eab814
Zikliz2213 <gurkosemen40@gmail.com>
2026-05-30 16:06:05 +0300

    api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,7 @@ const API_URL = '33221133-production.up.railway.app';
 // ======================================================
 // MOCK MODE — поменяй на true чтобы работать без сервера
 // ======================================================
-export const USE_MOCK = false;
+export const USE_MOCK = true;
 
 const mockDelay = () => new Promise(res => setTimeout(res, 600));
 
